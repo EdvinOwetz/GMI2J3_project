be0ec217e579e216eb2ce56d406380f93765f37e
Philip Sinnott <philipsinnotts@gmail.com>
2022-05-09 16:27:46 +0200

    Added oogas

--- Login-service/Login-service/obj/Debug/net6.0-windows/Views/AccountManagementView.g.i.cs
+++ Login-service/Login-service/obj/Debug/net6.0-windows/Views/AccountManagementView.g.i.cs
@@ -56,7 +56,7 @@ namespace Login_service.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Login_Service;component/views/accountmanagementview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Login_Service;V1.0.0.0;component/views/accountmanagementview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\AccountManagementView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
